5018ac00cb698e4c3854f74b0472bf70158dc9f0
aledam-12 <alessandrodambrosio1212@gmail.com>
2024-05-04 15:20:33 +0200

    Organizzazione in versioni
    In questo branch i file sono organizzati in versioni, per avere uno storico delle varie tasks

---
+++ beans - Copia/consoleBean.java
@@ -0,0 +1,29 @@
+package beans;
+import java.io.Serializable;
+
+public class consoleBean implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    String nome;
+
+    public consoleBean()
+        {
+            nome = " ";
+        }
+    
+    public String getNome()
+        {
+            return nome;
+        }
+    public void setNome(String nome)
+        {
+            this.nome = nome;
+        }
+
+    @Override
+    public String toString()
+        {
+            return nome;
+        }
+}
